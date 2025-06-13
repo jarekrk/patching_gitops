71a287a989eaa82c1b2672c99f1cd6d0a48ff194
jarekrk <jrozentalski@gmail.com>
2025-06-13 14:17:54 +0200

    change flask port

--- todo-app.py
+++ todo-app.py
@@ -68,5 +68,6 @@ def delete_task(task_id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 8080))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
